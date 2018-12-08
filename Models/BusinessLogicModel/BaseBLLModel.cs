accf3991941ef4e1c8151cf9f150950989f17253
Supanat Thanakarnchot <supanat_thana@hotmail.com>
2018-12-08 11:45:39 +0700

    Initial commit

---
+++ Models/BusinessLogicModel/BaseBLLModel.cs
@@ -0,0 +1,16 @@
+﻿using MiniFinance.Controllers;
+using MiniFinance.Models.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiniFinance.Models.BusinessLogicModel
+{
+    public class BaseBLLModel
+    {
+        private readonly static BaseController objBaseController = new BaseController();
+        protected readonly DateTime DateTimeNow = objBaseController.DateTimeNow;
+        protected ACCOUNTINGEntities _dbContext = new ACCOUNTINGEntities();
+    }
+}
